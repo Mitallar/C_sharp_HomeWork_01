50c054195361ea4a013a202bdb99077d8f3af140
Sheveliow Igor <strelanedura@list.ru>
2023-07-22 12:05:44 +0300

    Added hometasks

---
+++ hometask1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа: ");
+int number = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+if(number > number2)
+{
+    Console.WriteLine($"max = {number}");
+}
+if(number2 > number)
+{
+    Console.WriteLine($"max = {number2}");
+}
